64bbc04408a4bc8cad7d8182cd8b83bf9bc8d6fe
GiovanniBestoso <115479602+GiovanniBestoso@users.noreply.github.com>
2022-11-24 20:23:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main() {
-    int n=0, primo=1, secondo=1, terzo=0;
-    cin>>n;
-    if (n>2){
-        cout<<primo<<endl<<secondo<<endl;
-        for (int i = 0; i < (n-2); ++i) {
-            terzo=primo+secondo;
-            cout<<terzo<<endl;
-            primo=secondo;
-            secondo=terzo;
-        }
-    }else if (n==2){
-        cout<<primo<<endl<<secondo<<endl;
-    }else if(n==1){
-        cout<<primo<<endl;}
-    return 0;
+    int n = 0, primo = 1, secondo = 1, terzo = 0;
+     cin >> n;
+     if (n > 2) {
+         cout << primo << endl << secondo << endl;
+         for (int i = 0; i < (n - 2); ++i) {
+             terzo = primo + secondo;
+             cout << terzo << endl;
+             primo = secondo;
+             secondo = terzo;
+         }
+     } else if (n == 2) {
+         cout << primo << endl << secondo << endl;
+     } else if (n == 1) {
+         cout << primo << endl;
+         return 0;
+     }
 }
 
